b104dea5d6d99c8b61d1295ba066c6904ad2b4c1
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-04 15:36:47 +0900

    feat: perspective camera 및 render 기본 셋팅

--- project01/chapter02-threejs/main.js
+++ project01/chapter02-threejs/main.js
@@ -1,2 +1,33 @@
 import "./style.css";
 import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  60, //fov
+  window.innerWidth / window.innerHeight,
+  0.1, //near
+  100 //far
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 5;
+
+const geometry = new THREE.BoxGeometry(1,1,1); //가로, 세로, 높이
+const material = new THREE.MeshBasicMaterial({color:0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => {
+  // 리사이즈 시 화면과 물체의 크기가 이상해 지지 않도록 하는 기본 셋팅
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.render(scene, camera);
+})
+
+renderer.render(scene, camera);
