5aa96b06a0eaddca5a428aac95078a0ba05c3e01
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-05 10:56:28 +0900

    feat: Light 실습 종합

--- project01/chapter02-threejs/main.js
+++ project01/chapter02-threejs/main.js
@@ -18,134 +18,82 @@ camera.position.y = 5;
 camera.position.y = 5;
 camera.position.z = 5;
 
-// 직사광선
-const directionalLight = new THREE.DirectionalLight(0xffffff, 5); //빛의 색, 세기
-directionalLight.castShadow = true; // 빛의 그림자 설정
-directionalLight.position.set(3, 4, 5);
-directionalLight.lookAt(0, 0, 0);
-scene.add(directionalLight);
+//** Directional Light */
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+// directionalLight.castShadow = true;
+// directionalLight.position.set(3, 4, 5);
+// directionalLight.lookAt(0, 0, 0);
+// scene.add(directionalLight);
+
+// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1);
+// scene.add(directionalLightHelper)
+
+
+//** Ambient Light */
+// const ambientLight = new THREE.AmbientLight(0xffffff, 5);
+// scene.add(ambientLight)
+
+
+//** Hemisphere Light */
+// const hemisphereLight = new THREE.HemisphereLight(0xb4a912, 0x12f34f, 5);
+// hemisphereLight.position.set(0, 1, 0);
+// hemisphereLight.lookAt(0, 0, 0);
+// scene.add(hemisphereLight)
+
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 1);
+// scene.add(hemisphereLightHelper)
+
+
+//** Point Light */
+// const pointLight = new THREE.PointLight(0xffffff, 5, 5, 4);
+// pointLight.castShadow = true;
+// pointLight.position.set(1, 1, 1);
+// scene.add(pointLight)
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
+// scene.add(pointLightHelper)
+
+
+//** Rect Area Light */
+// const rectAreaLight = new THREE.RectAreaLight(0xffffff, 5, 2, 2);
+// rectAreaLight.position.set(0, 1, 2);
+// scene.add(rectAreaLight)
+
+
+//** Spot Light */
+const targetObj = new THREE.Object3D();
+scene.add(targetObj);
+
+const spotLight = new THREE.SpotLight(0xffffff, 10, 100, Math.PI/4, 1, 1);
+spotLight.castShadow = true;
+spotLight.position.set(0, 3, 0);
+spotLight.target = targetObj;
+spotLight.target.position.set(1, 0, 2);
+scene.add(spotLight)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(spotLightHelper)
+
 
 // 평면 Geometry (바닥)
 const floorGeometry = new THREE.PlaneGeometry(20, 20);
-const floorMaterial = new THREE.MeshStandardMaterial({color: 0xbbbbbb});
+const floorMaterial = new THREE.MeshStandardMaterial({
+  color: 0xbbbbbb,
+  // side: THREE.DoubleSide
+});
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
 floor.rotation.x = -Math.PI / 2;
 floor.receiveShadow = true;
 scene.add(floor)
 
-// Mesh FrontSide
-const frontSideGeometry = new THREE.BoxGeometry(1, 1, 1);
-const frontSideMaterial = new THREE.MeshStandardMaterial({
-  color: 0x00ffff,
-  side: THREE.FrontSide,
-});
-const frontSideMesh = new THREE.Mesh(frontSideGeometry, frontSideMaterial);
-frontSideMesh.position.z = 4;
-frontSideMesh.position.y = 0.5;
-frontSideMesh.castShadow = true;
-frontSideMesh.receiveShadow = true;
-scene.add(frontSideMesh);
-
-// Mesh BackSide
-const backSideGeometry = new THREE.BoxGeometry(1, 1, 1);
-const backSideMaterial = new THREE.MeshStandardMaterial({
-  color: 0x00ff00,
-  side: THREE.BackSide,
-});
-const backSideMesh = new THREE.Mesh(backSideGeometry, backSideMaterial);
-backSideMesh.position.set(2, 0.5, 4);
-backSideMesh.position.y = 0.51;
-backSideMesh.receiveShadow = true;
-scene.add(backSideMesh);
-
-// Mesh DoubleSide
-const doubleSideGeometry = new THREE.BoxGeometry(1, 1, 1);
-const doubleSideMaterial = new THREE.MeshStandardMaterial({
-  color: 0x00ff00,
-  side: THREE.DoubleSide,
-});
-const doubleSideMesh = new THREE.Mesh(doubleSideGeometry, doubleSideMaterial);
-doubleSideMesh.position.set(4, 0.5, 4);
-doubleSideMesh.position.y = 0.51;
-doubleSideMesh.receiveShadow = true;
-scene.add(doubleSideMesh);
-
-// TorusKnot
-const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 20);
-//** Mesh Standard Material */
-const torusKnotStandMaterial = new THREE.MeshStandardMaterial({color: 0xff0000});
-torusKnotStandMaterial.roughness = 0.5;
-torusKnotStandMaterial.metalness = 1;
-
-const torusKnotStandardMesh = new THREE.Mesh(torusKnotGeometry, torusKnotStandMaterial);
-torusKnotStandardMesh.position.set(-4, 1, 0);
-torusKnotStandardMesh.castShadow = true;
-torusKnotStandardMesh.receiveShadow = true;
-scene.add(torusKnotStandardMesh)
-
-//** Mesh Lambert Material */ 
-const torusKnotLambertMaterial = new THREE.MeshLambertMaterial({color:0xff0000});
-torusKnotLambertMaterial.emissive = new THREE.Color(0x00ff00);
-torusKnotLambertMaterial.emissiveIntensity = 0.2;
-
-const torusKnotLambertMesh = new THREE.Mesh(torusKnotGeometry, torusKnotLambertMaterial);
-torusKnotLambertMesh.position.set(-2, 1, 0);
-torusKnotLambertMesh.castShadow = true;
-torusKnotLambertMesh.receiveShadow = true;
-scene.add(torusKnotLambertMesh)
-
-//** Mesh Phong Material */ 
-const torusKnotPhongMaterial = new THREE.MeshPhongMaterial({color:0xff0000});
-torusKnotPhongMaterial.emissive = new THREE.Color(0x00ff00);
-torusKnotPhongMaterial.emissiveIntensity = 0.2;
-torusKnotPhongMaterial.specular = new THREE.Color(0x0000ff);
-torusKnotPhongMaterial.shininess = 100;
-
-const torusKnotPhongMesh = new THREE.Mesh(torusKnotGeometry, torusKnotPhongMaterial);
-torusKnotPhongMesh.position.set(0, 1, 0);
-torusKnotPhongMesh.castShadow = true;
-torusKnotPhongMesh.receiveShadow = true;
-scene.add(torusKnotPhongMesh)
-
-//** Mesh Basic Material */ 
-const torusKnotBasicMaterial = new THREE.MeshBasicMaterial({color:0xff0000});
-const torusKnotBasicMesh = new THREE.Mesh(torusKnotGeometry, torusKnotBasicMaterial);
-torusKnotBasicMesh.position.set(2, 1, 0);
-torusKnotBasicMesh.castShadow = true;
-torusKnotBasicMesh.receiveShadow = true;
-scene.add(torusKnotBasicMesh)
-
-//** Mesh Depth Material */ 
-const torusKnotDepthMaterial = new THREE.MeshDepthMaterial({color:0xffffff});
-torusKnotDepthMaterial.opacity = 0.5;
-const torusKnotDepthMesh = new THREE.Mesh(torusKnotGeometry, torusKnotDepthMaterial);
-torusKnotDepthMesh.position.set(4, 1, 0);
-torusKnotDepthMesh.castShadow = true;
-torusKnotDepthMesh.receiveShadow = true;
-scene.add(torusKnotDepthMesh)
-
-// Texture Loader
-const textureLoader = new THREE.TextureLoader();
-//** 동기 방식 */
-textureLoader.load("/threejs.webp", (texture) => {
-  const textureBoxGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const textureMaterial = new THREE.MeshStandardMaterial({map: texture});
-  const textureMesh = new THREE.Mesh(textureBoxGeometry, textureMaterial);
-  textureMesh.position.set(0, 0.5, 2);
-  textureMesh.castShadow = true;
-  textureMesh.receiveShadow = true;
-  scene.add(textureMesh)
-})
-//** 비동기 방식 */
-const texture = await textureLoader.loadAsync('/threejs.webp');
-const textureBoxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const textureMaterial = new THREE.MeshStandardMaterial({map: texture});
-const textureMesh = new THREE.Mesh(textureBoxGeometry, textureMaterial);
-textureMesh.position.set(2, 0.5, 2);
-textureMesh.castShadow = true;
-textureMesh.receiveShadow = true;
-scene.add(textureMesh)
-
+// 육면체
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({color:0xffffff});
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+boxMesh.castShadow = true;
+boxMesh.receiveShadow = true;
+boxMesh.position.y = 0.51;
+scene.add(boxMesh);
 
 // 마우스에 따른 카메라 시점 변경
 const orbitControls = new OrbitControls(camera, renderer.domElement);
