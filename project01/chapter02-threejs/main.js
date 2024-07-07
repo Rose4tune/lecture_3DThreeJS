074da6fce6be22d8fbe446b105ba3c991b7d6802
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-07 17:52:49 +0900

    feat: GLTF 실습 캐릭터 로드

--- project01/chapter02-threejs/main.js
+++ project01/chapter02-threejs/main.js
@@ -1,10 +1,9 @@
 import "./style.css";
 import * as THREE from "three";
+
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { FlyControls } from "three/examples/jsm/controls/FlyControls";
-import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls";
-import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
-import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.shadowMap.enabled = true; // 그림자 활성화
@@ -19,10 +18,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1, //near
   100 //far
 );
-// camera.position.y = 5;
-// camera.position.y = 5;
-// camera.position.z = 5;
-camera.position.set(0, 1, 0);
+camera.position.y = 5;
+camera.position.y = 5;
+camera.position.z = 5;
 
 //** Directional Light */
 const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
@@ -44,8 +42,34 @@ directionalLight.shadow.camera.near = 0.1;
 directionalLight.shadow.camera.far = 100;
 
 const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1);
-scene.add(directionalLightHelper)
+// scene.add(directionalLightHelper)
 
+//** GLTFLoader */
+const glrfLoader = new GLTFLoader();
+// 동기
+// glrfLoader.load("/dancer.glb", (data) => {
+//   const chracter = data.scene;
+//   chracter.position.y = 0.8
+//   chracter.scale.set(0.01, 0.01, 0.01)
+//   scene.add(chracter)
+// })
+
+// 비동기
+const gltf = await glrfLoader.loadAsync("/dancer.glb");
+const chracter = gltf.scene;
+chracter.position.y = 0.8;
+chracter.scale.set(0.01, 0.01, 0.01);
+chracter.castShadow = true;
+chracter.receiveShadow = true;
+chracter.traverse(obj => {
+  // children을 타고 내려가서 모든 요소에 그림자 설정 할 수 있게 함
+  if(obj.isMesh) {
+    obj.castShadow = true;
+    obj.receiveShadow = true;
+  }
+})
+console.log(gltf)
+scene.add(chracter)
 
 // 평면 Geometry (바닥)
 const floorGeometry = new THREE.PlaneGeometry(20, 20);
@@ -62,63 +86,12 @@ const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 boxMesh.castShadow = true;
 boxMesh.receiveShadow = true;
 boxMesh.position.y = 0.51;
-scene.add(boxMesh);
+// scene.add(boxMesh);
 
 //** Orbit Controls : 마우스에 따른 카메라 시점 변경 */
-// const orbitControls = new OrbitControls(camera, renderer.domElement);
-// orbitControls.enableDamping = true;
-// orbitControls.dampingFactor = 0.03;
-// orbitControls.enableZoom = true;
-// orbitControls.enablePan = true;
-// orbitControls.enableRotate = true;
-// orbitControls.autoRotate = false;
-// orbitControls.autoRotateSpeed = 2;
-
-// orbitControls.maxPolarAngle = Math.PI / 2;
-// orbitControls.minPolarAngle = Math.PI / 4;
-// orbitControls.maxAzimuthAngle = Math.PI / 2;
-// orbitControls.minAzimuthAngle = -Math.PI / 2;
-
-
-//** Fly Controls */
-// const flyControls = new FlyControls(camera, renderer.domElement);
-// flyControls.movementSpeed = 1;
-// flyControls.rollSpeed = Math.PI / 10;
-// flyControls.autoForward = false;
-
-
-//** FirstPersonControls */
-// const firstPersonControls = new FirstPersonControls(camera, renderer.domElement);
-// firstPersonControls.lookSpeed = 0.1; // 카메라 회전 스피드
-// firstPersonControls.movementSpeed = 1; // 카메라 이동 스피드
-// firstPersonControls.lookVertical = false;
-
-
-//** Pointer Lock Controls */
-// const pointerLockControls = new PointerLockControls(camera, renderer.domElement);
-// window.addEventListener('click', () => {
-//   pointerLockControls.lock();
-// })
-
-
-//** Trackball Controls */
-const trackballControls = new TrackballControls(camera, renderer.domElement);
-trackballControls.rotateSpeed = 2;
-trackballControls.zoomSpeed = 1.5;
-trackballControls.panSpeed = 0.5;
-trackballControls.noRotate = false;
-trackballControls.noZoom = false;
-trackballControls.noPan = false;
-trackballControls.staticMoving = false; //deping 활성화
-trackballControls.dynamicDampingFactor = 0.05;
-
-const target = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5),
-  new THREE.MeshStandardMaterial({color:0x0000ff})
-);
-target.position.set(4, 0.5, 0)
-scene.add(target)
-trackballControls.target = target.position
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
+orbitControls.dampingFactor = 0.03;
 
 window.addEventListener('resize', () => {
   // 리사이즈 시 화면과 물체의 크기가 이상해 지지 않도록 하는 기본 셋팅
@@ -133,10 +106,7 @@ const clock = new THREE.Clock();
 const render = () => {
   renderer.render(scene, camera);
   requestAnimationFrame(render)
-  // orbitControls.update();
-  // flyControls.update(clock.getDelta());
-  // firstPersonControls.update(clock.getDelta());
-  trackballControls.update();
+  orbitControls.update();
 }
 
 render();
