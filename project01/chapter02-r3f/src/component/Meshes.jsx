f6b16c11c929a05cf8cf63ee15ad62d2e6a6c8a3
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-10 11:18:07 +0900

    feat: R3F drei 지원 Mesh 실습

--- project01/chapter02-r3f/src/component/Meshes.jsx
+++ project01/chapter02-r3f/src/component/Meshes.jsx
@@ -4,10 +4,34 @@ import * as THREE from 'three';
 export const Meshes = () => {
   return (
     <>
-      <mesh position={[1, 0, 0]}>
-        <boxGeometry args={[1, 1, 1]}/>
-        <meshBasicMaterial color={0xff0000}/>
-      </mesh>
+      <Plane args={[40, 40]} rotation-x={-Math.PI / 2} receiveShadow>
+        <meshStandardMaterial/>
+      </Plane>
+      <Box args={[1, 1, 1]} castShadow position-y={0.5}>
+        <meshStandardMaterial color={0xff0000}/>
+      </Box>
+      <Sphere args={[1]} position={[0, 1, 3]} material-color={0xffff00} />
+      <Circle
+        args={[1]}
+        position={[0, 1, -3]}
+        material-color={"violet"}
+        material-side={THREE.DoubleSide}
+      />
+      <Cone
+        args={[1,2]}
+        position={[3, 1, 3]}
+        material-color={"brown"}
+      />
+      <Cylinder
+        args={[2, 1, 2]}
+        position={[3, 1, -3]}
+        material-color={"pink"}
+      />
+      <Torus
+        args={[1, 0.2]}
+        position={[-3, 1.2, -3]}
+        material-color={"hotpink"}
+      />
     </>
   )
 }
