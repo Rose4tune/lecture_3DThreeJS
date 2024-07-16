846e43202addf10a525316cb2a472d1842a5bf91
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-16 16:25:05 +0900

    feat: gsap 이용하여 카메라 조작하기

--- project01/chapter03-scroll-dancer/src/components/Dancer.jsx
+++ project01/chapter03-scroll-dancer/src/components/Dancer.jsx
@@ -1,10 +1,13 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import { useRecoilValue } from "recoil";
+import gsap from "gsap";
 import { IsEnteredAtom } from "../stores";
 import { Loader } from "./Loader";
+import { useThree } from "@react-three/fiber";
 
 export default function Dancer() {
+  const three = useThree();
   const isEntered = useRecoilValue(IsEnteredAtom);
   const dancerRef = useRef(null);
   const { scene, animations } = useGLTF("/models/dancer.glb");
@@ -13,7 +16,34 @@ export default function Dancer() {
   useEffect(() => {
     if (!isEntered) return;
     actions["wave"].play();
-  }, [actions]);
+  }, [actions, isEntered]);
+
+  useEffect(() => {
+    if (!isEntered) return;
+    if (!dancerRef.current) return;
+    gsap.fromTo(
+      three.camera.position,
+      {
+        x: -5,
+        y: 5,
+        z: 5,
+      },
+      {
+        duration: 2.5,
+        x: 0,
+        y: 6,
+        z: 12,
+      }
+    );
+    gsap.fromTo(
+      three.camera.rotation,
+      { z: Math.PI },
+      {
+        duration: 2.5,
+        z: 0,
+      }
+    );
+  }, [isEntered, three.camera.position]);
 
   if (isEntered) {
     return (
