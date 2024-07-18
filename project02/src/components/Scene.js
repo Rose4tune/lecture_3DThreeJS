938ab77541c7ccd61f12411ac09c3a5f7d271f97
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-18 21:02:57 +0900

    feat: openWeather API 호출, 환경변수 추가

--- project02/src/components/Scene.js
+++ project02/src/components/Scene.js
@@ -1,11 +1,17 @@
+import { useEffect } from "react";
 import Earth from "./Earth";
 import Weather from "./Weather";
+import { getCurrentWeather } from "../utils/weather";
 
 export default function Scene() {
+  useEffect(() => {
+    getCurrentWeather(44.34, 10.99, process.env.REACT_APP_API_KEY);
+  });
+
   return (
     <>
       <Earth position={[0, -0.7, -2]} />
-      <Weather Weather={"clear"} />
+      <Weather weather={"clear"} />
     </>
   );
 }
