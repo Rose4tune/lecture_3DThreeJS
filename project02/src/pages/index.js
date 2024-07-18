c0b6bf43c1da06b4fcb0e2b0d299ab082e26caa0
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-18 22:18:09 +0900

    feat: Suspense 사용하여 간단한 Loader 만들기

--- project02/src/pages/index.js
+++ project02/src/pages/index.js
@@ -1,13 +1,31 @@
 import { Canvas } from "@react-three/fiber";
 import Lights from "../components/Lights";
-import Scene from "../components/Scene";
+// import Scene from "../components/Scene";
+import { lazy, Suspense } from "react";
+
+function Sphere() {
+  return (
+    <mesh>
+      <sphereGeometry args={[1]} />
+      <meshBasicMaterial color={"pink"} />
+    </mesh>
+  );
+}
+
+const Scene = lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(import("../components/Scene")), 2000);
+  });
+});
 
 export default function Home() {
   return (
     <Canvas camera={{ position: [0, 0, 2], fov: 45 }}>
       <color attach="background" args={["rgb(67, 127, 240) 100%)"]} />
-      <Lights />
-      <Scene />
+      <Suspense fallback={<Sphere />}>
+        <Lights />
+        <Scene />
+      </Suspense>
     </Canvas>
   );
 }
