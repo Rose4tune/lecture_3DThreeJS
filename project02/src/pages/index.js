21d287cead2b01882cfb8438bae0c57eb06b9d63
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-20 12:55:10 +0900

    feat: framer-motion-3d 실습

--- project02/src/pages/index.js
+++ project02/src/pages/index.js
@@ -2,6 +2,37 @@ import { Canvas } from "@react-three/fiber";
 import Lights from "../components/Lights";
 // import Scene from "../components/Scene";
 import { lazy, Suspense } from "react";
+import { motion } from "framer-motion-3d";
+
+function FrameworkModel() {
+  return (
+    <motion.mesh
+      initial={{ rotateX: Math.PI / 2, rotateZ: 1, x: 0, y: 0, z: -3 }}
+      animate={{
+        rotateZ: [0, 1, 4, Math.PI],
+        scale: [0.5, 1, 0.5],
+        x: 0,
+        y: 1,
+        z: 0,
+        transition: { duration: 2, delay: 1, type: "spring" },
+      }}
+      transition={{ duration: 3, repeat: Infinity }}
+    >
+      <cylinderGeometry args={[1, 1, 0.5, 8]} />
+      <motion.meshBasicMaterial
+        initial={{ opacity: 1 }}
+        color={"hotpink"}
+        animate={{
+          opacity: [1, 0.5, 1],
+          transition: {
+            duration: 0.5,
+            repeat: Infinity,
+          },
+        }}
+      />
+    </motion.mesh>
+  );
+}
 
 function Sphere() {
   return (
@@ -24,7 +55,8 @@ export default function Home() {
       <color attach="background" args={["rgb(67, 127, 240) 100%)"]} />
       <Suspense fallback={<Sphere />}>
         <Lights />
-        <Scene />
+        {/* <Scene /> */}
+        <FrameworkModel />
       </Suspense>
     </Canvas>
   );
