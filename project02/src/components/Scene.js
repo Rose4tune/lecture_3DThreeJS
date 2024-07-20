2cc3b2220152949c9d2e1a8e75386f1bbd613b5d
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-20 21:26:15 +0900

    feat: 날씨 컴포넌트 포커스 애니메이션 구현

--- project02/src/components/Scene.js
+++ project02/src/components/Scene.js
@@ -3,6 +3,8 @@ import Earth from "./Earth";
 import Weather from "./Weather";
 import { getCityWeather } from "../utils/weather";
 import { cities } from "../utils/cities";
+import { Bounds } from "@react-three/drei";
+import FocuseWeather from "./FocuseWeather";
 
 const API = process.env.REACT_APP_API_KEY;
 
@@ -26,22 +28,26 @@ export default function Scene() {
   return (
     <>
       <Earth />
-      {content?.map(({ city, weatherData }, i) => {
-        const angle = (i / (content.length - 1)) * (Math.PI / 1.5) + 0.55;
-        const radius = 1.3;
+      <Bounds fit clip observe margin={0.6}>
+        <FocuseWeather>
+          {content?.map(({ city, weatherData }, i) => {
+            const angle = (i / (content.length - 1)) * (Math.PI / 1.5) + 0.55;
+            const radius = 1.3;
 
-        const x = radius * Math.cos(angle).toFixed(2);
-        const y = radius * Math.sin(angle).toFixed(2);
-        return (
-          <Weather
-            key={i + "ModelKey"}
-            position={[x, y - 0.9, 0]}
-            rotationY={i + 100}
-            cityName={city}
-            weather={weatherData.weather[0].main.toLowerCase()}
-          />
-        );
-      })}
+            const x = radius * Math.cos(angle).toFixed(2);
+            const y = radius * Math.sin(angle).toFixed(2);
+            return (
+              <Weather
+                key={i + "ModelKey"}
+                position={[x, y - 0.9, 0]}
+                rotationY={i + 100}
+                cityName={city}
+                weather={weatherData.weather[0].main.toLowerCase()}
+              />
+            );
+          })}
+        </FocuseWeather>
+      </Bounds>
     </>
   );
 }
