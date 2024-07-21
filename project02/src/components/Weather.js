ed0373d80ea1324ff297a396a9243e00189447d8
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-21 12:23:24 +0900

    feat: navigate로 상세페이지 이동 연결하기

--- project02/src/components/Weather.js
+++ project02/src/components/Weather.js
@@ -3,13 +3,14 @@ import { useFrame } from "@react-three/fiber";
 import { useMemo, useRef, useState } from "react";
 import { motion } from "framer-motion-3d";
 import CityName from "./CityName";
+import { useNavigate } from "react-router-dom";
 
 export default function Weather(props) {
   const { position, weather, rotationY, cityName } = props;
   const { nodes } = useGLTF("/models/weather.glb");
-
   const weatherRef = useRef(null);
   const [isHover, setIsHover] = useState(false);
+  const navigate = useNavigate();
 
   const weatherModel = useMemo(() => {
     const cloneModel = nodes[weather] || nodes.cloud;
@@ -20,12 +21,20 @@ export default function Weather(props) {
     weatherRef.current.rotation.y += delta;
   });
 
+  const farmatCityName = (name) => {
+    return name.replace(/\s/g, "").toLowerCase();
+  };
+  const onClick = () => {
+    navigate(`/${farmatCityName(cityName)}`);
+  };
+
   return (
     <group position={position} rotation-y={rotationY}>
       <motion.mesh
         ref={weatherRef}
         onPointerEnter={() => setIsHover(true)}
         onPointerLeave={() => setIsHover(false)}
+        onClick={onClick}
         whileHover={{
           scale: 1.5,
           transition: 0.5,
