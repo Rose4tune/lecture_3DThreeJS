d860cfebcebe7055091f36ac5ac6284adbcc675f
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-23 13:00:46 +0900

    comment: 실습 파일 및 코드 삭제

--- project03/src/Scene.jsx
+++ project03/src/Scene.jsx
@@ -1,36 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import { Ground } from "./Ground";
 import { Debug, Physics } from "@react-three/cannon";
-import { useControls } from "leva";
-import Box from "./Box";
-import Sphere from "./Sphere";
-import Cylinder from "./Cylinder";
-import Torus from "./Torus";
-import Icosahedron from "./Icosahedron";
 
 function Scene() {
-  const bgValue = useControls({ bgColor: "#fff" });
-
-  const gravity = useControls("Gravity", {
-    x: { value: 0, min: -10, max: 10, step: 0.1 },
-    y: { value: -9.8, min: -10, max: 10, step: 0.1 },
-    z: { value: 0, min: -10, max: 10, step: 0.1 },
-  });
-
   return (
     <>
       <Canvas camera={{ position: [0, 2, 4] }}>
-        <color attach={"background"} args={[bgValue.bgColor]} />
-        <Physics gravity={[gravity.x, gravity.y, gravity.z]}>
+        <ambientLight />
+        <directionalLight position={[0, 5, 5]} />
+        <Physics gravity={[0, -2.6, 0]}>
           <Debug>
-            <ambientLight />
-            <directionalLight position={[0, 5, 5]} />
-            <Box position={[0, 1, 0]} />
-            <Sphere position={[2, 1, 0]} />
-            <Cylinder position={[-2, 1, 0]} />
             <Ground rotation={[-Math.PI / 2, 0, 0]} />
-            <Torus position={[-2, 1, 2]} />
-            <Icosahedron position={[1, 1, 2]} />
           </Debug>
         </Physics>
       </Canvas>
