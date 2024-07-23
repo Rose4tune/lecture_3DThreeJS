255b228185d2a060dc1ff9810e249c1a8b937f3f
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-23 14:04:00 +0900

    feat: 자동차 바퀴 만들기 (useCompoundBody, useRaycastVehicle)

--- project03/src/Car.jsx
+++ project03/src/Car.jsx
@@ -1,6 +1,9 @@
 import { useControls } from "leva";
 import DummyCarBody from "./dummy/DummyCarBody";
-import { useBox } from "@react-three/cannon";
+import { useCompoundBody, useRaycastVehicle } from "@react-three/cannon";
+import { useRef } from "react";
+import useWheels from "./utils/useWheels";
+import DummyWheel from "./dummy/DummyWheel";
 
 export default function Car() {
   const chassisBodyValue = useControls("chassisBody", {
@@ -11,23 +14,49 @@ export default function Car() {
 
   const position = [0, 0.5, 0];
 
-  let width, height, front, mass;
+  let width, height, front, mass, wheelRadius;
 
   width = 0.16;
   height = 0.12;
   front = 0.17;
   mass = 150;
+  wheelRadius = 0.05;
 
   const chassisBodyArgs = [width, height, front * 2];
 
-  const [chassisBody, chassisApi] = useBox(() => ({
-    args: chassisBodyArgs,
-    position,
-    mass,
-  }));
+  const [chassisBody, chassisApi] = useCompoundBody(
+    () => ({
+      position,
+      mass,
+      shapes: [
+        {
+          args: chassisBodyArgs,
+          position: [0, 0, 0],
+          type: "Box",
+        },
+        {
+          args: [width, height, front],
+          position: [0, height, 0],
+          type: "Box",
+        },
+      ],
+    }),
+    useRef(null)
+  );
+
+  const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius);
+
+  const [vehicle, vehicleApi] = useRaycastVehicle(
+    () => ({
+      chassisBody,
+      wheelInfos,
+      wheels,
+    }),
+    useRef(null)
+  );
 
   return (
-    <group>
+    <group ref={vehicle}>
       <group ref={chassisBody}>
         <DummyCarBody
           width={chassisBodyValue.width}
@@ -35,6 +64,10 @@ export default function Car() {
           front={chassisBodyValue.front}
         />
       </group>
+      <DummyWheel wheelRef={wheels[0]} radius={wheelRadius} />
+      <DummyWheel wheelRef={wheels[1]} radius={wheelRadius} />
+      <DummyWheel wheelRef={wheels[2]} radius={wheelRadius} />
+      <DummyWheel wheelRef={wheels[3]} radius={wheelRadius} />
     </group>
   );
 }
---
+++ project03/src/dummy/DummyWheel.jsx
@@ -0,0 +1,10 @@
+export default function DummyWheel({ wheelRef, radius }) {
+  return (
+    <group ref={wheelRef}>
+      <mesh rotation={[0, 0, -Math.PI / 2]}>
+        <cylinderGeometry args={[radius, radius, 0.025, 16]} />
+        <meshNormalMaterial />
+      </mesh>
+    </group>
+  );
+}
