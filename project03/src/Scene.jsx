668391a2de23c329a5de7fb37b9c15adbda38ba4
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-23 17:02:47 +0900

    feat: 물리엔진 Static 실습

--- project03/src/Scene.jsx
+++ project03/src/Scene.jsx
@@ -5,6 +5,7 @@ import Car from "./Car";
 import DummyMovementArea from "./dummy/DummyMovementArea";
 import DummyBox from "./dummy/DummyBox";
 import DummyBall from "./dummy/DummyBall";
+import DummyWall from "./dummy/DummyWall";
 
 function Scene() {
   return (
@@ -18,6 +19,10 @@ function Scene() {
             <DummyMovementArea position={[0, 0.1, -2]} />
             <DummyBox position={[1, 0.2, -2]} args={[1, 1, 1]} />
             <DummyBall position={[0, 0.2, -2]} args={[0.15]} />
+            <DummyWall position={[5, 0.5, 0]} args={[1, 1, 10]} />
+            <DummyWall position={[0, 0.5, 5]} args={[10, 1, 1]} />
+            <DummyWall position={[0, 0.5, -5]} args={[10, 1, 1]} />
+            <DummyWall position={[-5, 0.5, 0]} args={[1, 1, 10]} />
             <Ground rotation={[-Math.PI / 2, 0, 0]} />
           </Debug>
         </Physics>
