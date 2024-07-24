c5c926e73c8d3a576b614c1f23531b81b7ea359f
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-24 22:42:07 +0900

    refactor: 물체들 Ground.jsx로 옮김

--- project03/src/Ground.jsx
+++ project03/src/Ground.jsx
@@ -1,5 +1,8 @@
-import React from "react";
 import { usePlane } from "@react-three/cannon";
+import DummyMovementArea from "./dummy/DummyMovementArea";
+import DummyBox from "./dummy/DummyBox";
+import DummyBall from "./dummy/DummyBall";
+import DummyWall from "./dummy/DummyWall";
 
 export function Ground(props) {
   const [meshRef] = usePlane(() => ({
@@ -10,9 +13,18 @@ export function Ground(props) {
   }));
 
   return (
-    <mesh {...props} ref={meshRef} receiveShadow>
-      <planeGeometry args={[15, 15]} />
-      <meshStandardMaterial color="black" wireframe />
-    </mesh>
+    <group>
+      <mesh {...props} ref={meshRef} receiveShadow>
+        <planeGeometry args={[15, 15]} />
+        <meshStandardMaterial color="black" wireframe />
+      </mesh>
+      <DummyMovementArea position={[0, 0.1, -2]} />
+      <DummyBox position={[1, 0.2, -2]} args={[1, 1, 1]} />
+      <DummyBall position={[2, 0.2, 1]} args={[0.15]} />
+      <DummyWall position={[5, 0.5, 0]} args={[1, 1, 10]} />
+      <DummyWall position={[0, 0.5, 5]} args={[10, 1, 1]} />
+      <DummyWall position={[0, 0.5, -5]} args={[10, 1, 1]} />
+      <DummyWall position={[-5, 0.5, 0]} args={[1, 1, 10]} />
+    </group>
   );
 }
--- project03/src/Scene.jsx
+++ project03/src/Scene.jsx
@@ -2,10 +2,6 @@ import { Canvas } from "@react-three/fiber";
 import { Ground } from "./Ground";
 import { Debug, Physics } from "@react-three/cannon";
 import Car from "./Car";
-import DummyMovementArea from "./dummy/DummyMovementArea";
-import DummyBox from "./dummy/DummyBox";
-import DummyBall from "./dummy/DummyBall";
-import DummyWall from "./dummy/DummyWall";
 
 function Scene() {
   return (
@@ -16,13 +12,6 @@ function Scene() {
         <Physics gravity={[0, -2.6, 0]}>
           <Debug>
             <Car />
-            <DummyMovementArea position={[0, 0.1, -2]} />
-            <DummyBox position={[1, 0.2, -2]} args={[1, 1, 1]} />
-            <DummyBall position={[2, 0.2, 1]} args={[0.15]} />
-            <DummyWall position={[5, 0.5, 0]} args={[1, 1, 10]} />
-            <DummyWall position={[0, 0.5, 5]} args={[10, 1, 1]} />
-            <DummyWall position={[0, 0.5, -5]} args={[10, 1, 1]} />
-            <DummyWall position={[-5, 0.5, 0]} args={[1, 1, 10]} />
             <Ground rotation={[-Math.PI / 2, 0, 0]} />
           </Debug>
         </Physics>
