7467e07aa6f414875bf432024fd029e1d9534e2e
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-28 23:30:55 +0900

    feat: socket client 기본 구현

--- project04/frontend/src/App.jsx
+++ project04/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { MainCanvas } from "./components/content/canvas/MainCanvas";
+import { ClientSocketControl } from "./components/utilComponents/ClientSocketControl";
 
 function App() {
   return (
     <>
       <MainCanvas />
+      <ClientSocketControl />
     </>
   );
 }
