ed55335291797c178e4461611ddf5800de5488db
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-30 22:37:19 +0900

    fix: player atom 수정

--- project04/frontend/src/store/PlayersAtom.js
+++ project04/frontend/src/store/PlayersAtom.js
@@ -1,16 +1,19 @@
 import { atom } from "recoil";
 
+// 내 socket 정보
 export const MeAtom = atom({
   key: "MeAtom",
   default: undefined,
 });
 
+// 캐릭터 선택이 완료되었는지,
 export const CharacterSelectFinishedAtom = atom({
   key: "CharacterSelectFinishedAtom",
   default: false,
 });
 
-export const selectedCharacterGlbNameIndex = atom({
-  key: "selectedCharacterGlbNameIndex",
+// 현재 선택된 캐릭터 종류
+export const SelectedCharacterGlbNameIndexAtom = atom({
+  key: "SelectedCharacterGlbNameIndexAtom",
   default: 0,
 });
