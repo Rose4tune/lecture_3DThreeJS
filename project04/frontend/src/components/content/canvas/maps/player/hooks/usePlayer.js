691b9515fb50dd68201e2cd2c12e02f1474be113
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-31 23:35:28 +0900

    feat: 캐릭터 이동기능 구현(click 이벤트)

--- project04/frontend/src/components/content/canvas/maps/player/hooks/usePlayer.js
+++ project04/frontend/src/components/content/canvas/maps/player/hooks/usePlayer.js
@@ -1,10 +1,14 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
-import { useGraph } from "@react-three/fiber";
+import { useFrame, useGraph } from "@react-three/fiber";
 import { useEffect, useMemo, useRef, useState } from "react";
+import { useRecoilValue } from "recoil";
 import { SkeletonUtils } from "three-stdlib";
+import { MeAtom } from "../../../../../../store/PlayersAtom";
 
 export const usePlayer = ({ player, position, modelIndex }) => {
   const playerId = player?.id;
+  const me = useRecoilValue(MeAtom);
+
   const memoizedPosition = useMemo(() => position, []);
 
   const playerRef = useRef(null);
@@ -40,6 +44,36 @@ export const usePlayer = ({ player, position, modelIndex }) => {
       actions[animation]?.fadeOut(0.5);
     };
   }, [actions, animation]);
+
+  useFrame(({ camera }) => {
+    if (!player) return;
+    if (!playerRef.current) return;
+    if (playerRef.current.position.distanceTo(position) > 0.1) {
+      const direction = playerRef.current.position
+        .clone()
+        .sub(position)
+        .normalize()
+        .multiplyScalar(0.04);
+
+      playerRef.current.position.sub(direction);
+      playerRef.current.lookAt(position);
+
+      setAnimation("CharacterArmature|CharacterArmature|CharacterArmature|Run");
+    } else {
+      setAnimation(
+        "CharacterArmature|CharacterArmature|CharacterArmature|Idle"
+      );
+    }
+
+    if (me?.id === playerId) {
+      camera.position.set(
+        playerRef.current.position.x + 12,
+        playerRef.current.position.y + 12,
+        playerRef.current.position.z + 12
+      );
+      camera.lookAt(playerRef.current.position);
+    }
+  });
 
   return { playerRef, memoizedPosition, playerId, nodes, materials };
 };
