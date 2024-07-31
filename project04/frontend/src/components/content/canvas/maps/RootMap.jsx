ed20a0945d1e351e0cbf0cb1f0444afeeffbaa5c
Lee Ye Seo <rosefor2ne@gmail.com>
2024-07-31 18:07:49 +0900

    feat: 운동장 맵 내 캐릭터 로드 구현

--- project04/frontend/src/components/content/canvas/maps/RootMap.jsx
+++ project04/frontend/src/components/content/canvas/maps/RootMap.jsx
@@ -1,12 +1,80 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { GroundElements } from "./structures/ground";
 import { useRecoilValue } from "recoil";
-import { CharacterSelectFinishedAtom } from "../../../../store/PlayersAtom";
+import {
+  CharacterSelectFinishedAtom,
+  PlayersAtom,
+} from "../../../../store/PlayersAtom";
 import { CharacterInit } from "../../lobby/CharacterInit";
+import { useThree } from "@react-three/fiber";
+import { Man } from "./player/Man";
+import { Woman } from "./player/Woman";
+import { Kid } from "./player/Kid";
+import { Vector3 } from "three";
 
 export const RootMap = () => {
   const characterSelectFinished = useRecoilValue(CharacterSelectFinishedAtom);
+  const players = useRecoilValue(PlayersAtom);
+  const camera = useThree((three) => three.camera);
+  const controls = useRef(null);
+
+  useEffect(() => {
+    if (!controls.current) return;
+    camera.position.set(14, 14, 14);
+    controls.current.target.set(0, 0, 0);
+  }, [camera.position]);
+
   return (
-    <>{!characterSelectFinished ? <CharacterInit /> : <GroundElements />}</>
+    <>
+      {!characterSelectFinished ? (
+        <CharacterInit />
+      ) : (
+        <>
+          <GroundElements />
+          {players.map((player) => {
+            return (
+              <>
+                {player.selectedCharacterGlbNameIndex === 0 && (
+                  <Man
+                    player={player}
+                    position={
+                      new Vector3(
+                        player.position[0],
+                        player.position[1],
+                        player.position[2]
+                      )
+                    }
+                  />
+                )}
+                {player.selectedCharacterGlbNameIndex === 1 && (
+                  <Woman
+                    player={player}
+                    position={
+                      new Vector3(
+                        player.position[0],
+                        player.position[1],
+                        player.position[2]
+                      )
+                    }
+                  />
+                )}
+                {player.selectedCharacterGlbNameIndex === 2 && (
+                  <Kid
+                    player={player}
+                    position={
+                      new Vector3(
+                        player.position[0],
+                        player.position[1],
+                        player.position[2]
+                      )
+                    }
+                  />
+                )}
+              </>
+            );
+          })}
+        </>
+      )}
+    </>
   );
 };
