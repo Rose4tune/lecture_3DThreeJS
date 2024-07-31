ecde3d1251097b2a8abefdb6dc1463d94a8c3642
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-01 00:16:55 +0900

    feat: 운동장 맵 구조물 추가

---
+++ project04/frontend/src/components/content/canvas/maps/structures/ground/elements/Tree.jsx
@@ -0,0 +1,27 @@
+import { useGLTF } from "@react-three/drei";
+import { useEffect, useMemo } from "react";
+import { SkeletonUtils } from "three-stdlib";
+
+const name = "ground-tree";
+
+export const Tree = ({ position }) => {
+  const { scene: scene_ } = useGLTF("/models/Tree.glb");
+  const scene = useMemo(() => SkeletonUtils.clone(scene_), []);
+
+  useEffect(() => {
+    scene.traverse((mesh) => {
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+    });
+  }, [position, scene]);
+
+  return (
+    <primitive
+      visible
+      name={name}
+      scale={1}
+      position={position}
+      object={scene}
+    />
+  );
+};
