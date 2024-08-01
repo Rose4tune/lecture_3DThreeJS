9d57a348d7e6608e60e380f3bbb642709475095c
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-01 12:50:59 +0900

    feat: 강아지 먹이주기 퀘스트 구현

--- project04/frontend/src/components/content/canvas/maps/structures/ground/elements/Steak.jsx
+++ project04/frontend/src/components/content/canvas/maps/structures/ground/elements/Steak.jsx
@@ -1,6 +1,8 @@
 import { useGLTF } from "@react-three/drei";
 import { useEffect, useMemo, useRef } from "react";
+import { useRecoilState } from "recoil";
 import { Vector3 } from "three";
+import { PlayerInventoryAtom } from "../../../../../../../store/PlayersAtom";
 import { uniq } from "lodash-es";
 
 const name = "ground-steak";
@@ -9,6 +11,9 @@ export const Steak = () => {
   const ref = useRef(null);
   const { scene } = useGLTF("/models/Steak.glb");
   const position = useMemo(() => new Vector3(-8, 0, -2), []);
+
+  const [playerInventory, setPlayerInventory] =
+    useRecoilState(PlayerInventoryAtom);
 
   useEffect(() => {
     scene.traverse((mesh) => {
@@ -19,6 +24,14 @@ export const Steak = () => {
 
   return (
     <primitive
+      onClick={(e) => {
+        e.stopPropagation();
+        alert("고기를 얻었습니다!");
+        setPlayerInventory((prev) => uniq([...prev, "food"]));
+        if (ref.current) {
+          ref.current.visible = false;
+        }
+      }}
       ref={ref}
       visible
       name={name}
--- project04/frontend/src/components/content/canvas/maps/structures/ground/elements/npc/ShibaInu.jsx
+++ project04/frontend/src/components/content/canvas/maps/structures/ground/elements/npc/ShibaInu.jsx
@@ -1,20 +1,33 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { Vector3 } from "three";
 import { TextBoard } from "../../3dUIs/TextBoard";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useAnimatedText } from "../../../../../../../hooks/useAnimatedText";
+import {
+  PlayerCompletedQuestsAtom,
+  PlayerInventoryAtom,
+} from "../../../../../../../../store/PlayersAtom";
+import { useRecoilState } from "recoil";
 
 const name = "ground-shiba-inu";
-const text = "멍멍! 내 고기가 어디갔지..?   ";
 
 export const ShibaInu = () => {
   const ref = useRef(null);
   const nameRef = useRef(null);
   const chatRef = useRef(null);
 
+  const [text, setText] = useState("멍멍! 내 고기가 어디갔지..?   ");
   const { displayText } = useAnimatedText(text);
+
+  const threeScene = useThree((three) => three.scene);
+  const [playerInventory, setPlayerInventory] =
+    useRecoilState(PlayerInventoryAtom);
+  const [playerCompletedQuests, setPlayerCompletedQuests] = useRecoilState(
+    PlayerCompletedQuestsAtom
+  );
+
   const { scene, animations } = useGLTF("/models/Shiba Inu.glb");
   const { actions } = useAnimations(animations, ref);
   const position = useMemo(() => new Vector3(-1, 0, 21), []);
@@ -33,28 +46,44 @@ export const ShibaInu = () => {
     });
 
     let animation;
-    actions["Walk"]?.play();
-    animation = gsap.to(ref.current.position, {
-      duration: 5,
-      yoyo: true,
-      repeat: -1,
-      x: 3,
-      ease: "linear",
-      onUpdate: () => {
-        const progress = animation.progress();
-        if (Math.abs(progress) < 0.01) {
-          ref.current?.lookAt(3, 0, 21);
-        } else if (Math.abs(progress) > 0.99) {
-          ref.current?.lookAt(-1, 0, 21);
-        }
-      },
-    });
-    animation?.play();
+    if (playerCompletedQuests.includes("dog")) {
+      setText("멍멍! 고마워!   ");
+      actions["Walk"]?.stop();
+      actions["Eating"]?.play();
+      ref.current?.lookAt(3, 0, 21);
+      const steak = threeScene.getObjectByName("ground-steak");
+      if (steak) {
+        steak.visible = true;
+        steak?.position.set(
+          ref.current.position.x + 1,
+          0,
+          ref.current.position.z
+        );
+      }
+    } else {
+      actions["Walk"]?.play();
+      animation = gsap.to(ref.current.position, {
+        duration: 5,
+        yoyo: true,
+        repeat: -1,
+        x: 3,
+        ease: "linear",
+        onUpdate: () => {
+          const progress = animation.progress();
+          if (Math.abs(progress) < 0.01) {
+            ref.current?.lookAt(3, 0, 21);
+          } else if (Math.abs(progress) > 0.99) {
+            ref.current?.lookAt(-1, 0, 21);
+          }
+        },
+      });
+      animation?.play();
+    }
 
     return () => {
       animation?.pause();
     };
-  }, [actions, position, scene]);
+  }, [actions, playerCompletedQuests, position, scene, threeScene]);
 
   useFrame(() => {
     if (!ref.current) return;
@@ -79,6 +108,18 @@ export const ShibaInu = () => {
       <TextBoard ref={chatRef} text={displayText} />
       <TextBoard ref={nameRef} text="PUPPI" isNpc />
       <primitive
+        onClick={(e) => {
+          e.stopPropagation();
+          if (playerInventory.includes("food")) {
+            alert("강아지에게 고기를 주었습니다. 강아지가 당신을 좋아합니다.");
+            setPlayerInventory((prev) =>
+              prev.filter((item) => item !== "food")
+            );
+            setPlayerCompletedQuests((prev) => [...prev, "dog"]);
+          } else {
+            alert("강아지에게 먹일 고기를 갖다주세요.");
+          }
+        }}
         ref={ref}
         scale={0.7}
         visible
