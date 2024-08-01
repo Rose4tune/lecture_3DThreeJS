47677f4abce5bf58318ec1332a5303cd9806df35
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-01 17:33:13 +0900

    design: sidebar ui 구현

--- project04/frontend/src/components/content/canvasLayout/Layout.jsx
+++ project04/frontend/src/components/content/canvasLayout/Layout.jsx
@@ -1,3 +1,21 @@
+import { useRecoilValue } from "recoil";
+import { IsLoadCompletedAtom } from "../../../store/PlayersAtom";
+import { styled } from "styled-components";
+import { SideBar } from "./canvas/UserInterfaces/common/SideBar";
+
 export const CanvasLayout = ({ children }) => {
-  return <>{children}</>;
+  const isLoadCompleted = useRecoilValue(IsLoadCompletedAtom);
+  return (
+    <Wrapper>
+      {children}
+      {isLoadCompleted && <SideBar />}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  background-color: transparent;
+  width: 100vw;
+  height: 100vh;
+`;
