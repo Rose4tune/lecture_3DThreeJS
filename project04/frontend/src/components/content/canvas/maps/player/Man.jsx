19b64991592bd594213ac963a50de72cad864174
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-01 10:48:06 +0900

    feat: 캐릭터 이름표 추가

--- project04/frontend/src/components/content/canvas/maps/player/Man.jsx
+++ project04/frontend/src/components/content/canvas/maps/player/Man.jsx
@@ -1,42 +1,57 @@
+import { NicknameBoard } from "../structures/ground/3dUIs/NicknameBoard";
 import { usePlayer } from "./hooks/usePlayer";
 
 export function Man({ player, position, modelIndex }) {
-  const { playerRef, memoizedPosition, playerId, nodes, materials } = usePlayer(
-    {
-      player,
-      position,
-      modelIndex: modelIndex ?? player.selectedCharacterGlbNameIndex,
-    }
-  );
+  const {
+    me,
+    playerRef,
+    nicknameRef,
+    memoizedPosition,
+    playerId,
+    nodes,
+    materials,
+  } = usePlayer({
+    player,
+    position,
+    modelIndex: modelIndex ?? player.selectedCharacterGlbNameIndex,
+  });
 
   return (
-    <group
-      ref={playerRef}
-      position={memoizedPosition}
-      name={playerId ?? ""}
-      dispose={null}
-    >
-      <group name="Root_Scene">
-        <group name="RootNode">
-          <group
-            name="CharacterArmature"
-            rotation={[-Math.PI / 2, 0, 0]}
-            scale={100}
-          >
-            <primitive object={nodes.Root} />
+    <>
+      {me && (
+        <NicknameBoard
+          ref={nicknameRef}
+          text={`${player?.nickname} ${player.jobPosition}`}
+        />
+      )}
+      <group
+        ref={playerRef}
+        position={memoizedPosition}
+        name={playerId ?? ""}
+        dispose={null}
+      >
+        <group name="Root_Scene">
+          <group name="RootNode">
+            <group
+              name="CharacterArmature"
+              rotation={[-Math.PI / 2, 0, 0]}
+              scale={100}
+            >
+              <primitive object={nodes.Root} />
+            </group>
+            <skinnedMesh
+              castShadow
+              receiveShadow
+              name="Character"
+              geometry={nodes.Character.geometry}
+              material={materials.Atlas}
+              skeleton={nodes.Character.skeleton}
+              rotation={[-Math.PI / 2, 0, 0]}
+              scale={100}
+            />
           </group>
-          <skinnedMesh
-            castShadow
-            receiveShadow
-            name="Character"
-            geometry={nodes.Character.geometry}
-            material={materials.Atlas}
-            skeleton={nodes.Character.skeleton}
-            rotation={[-Math.PI / 2, 0, 0]}
-            scale={100}
-          />
         </group>
       </group>
-    </group>
+    </>
   );
 }
