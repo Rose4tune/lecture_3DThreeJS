d4047b524968874d2bd15aded9191ea8522d8dcc
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-08 23:23:27 +0900

    feat: core 상단 텍스트 애니메이션

--- project05/src/components/sections/Core.jsx
+++ project05/src/components/sections/Core.jsx
@@ -1,17 +1,70 @@
+"use client";
+
+import { motion, useAnimationControls, useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
+
 export default function Core() {
+  const scrollRef = useRef();
+  const { scrollYProgress } = useScroll({
+    target: scrollRef,
+    offset: ["start end", "end start"],
+  });
+  const control1 = useAnimationControls();
+  const control2 = useAnimationControls();
+  const control3 = useAnimationControls();
+
+  useEffect(() => {
+    const onScrollChange = (yProgress) => {
+      if (yProgress < 0.2) {
+        control1.start({ opacity: 0, y: 20 });
+      } else if (yProgress < 0.3) {
+        control1.start({ opacity: 1, y: 0 });
+        control2.start({ opacity: 0, y: 20 });
+      } else if (yProgress < 0.35) {
+        control1.start({ opacity: 1, y: 0 });
+        control2.start({ opacity: 1, y: 0 });
+        control3.start({ opacity: 0, y: 20 });
+      } else {
+        control1.start({ opacity: 1, y: 0 });
+        control2.start({ opacity: 1, y: 0 });
+        control3.start({ opacity: 1, y: 0 });
+      }
+    };
+    const unsubscribeY = scrollYProgress.on("change", onScrollChange);
+
+    return () => {
+      unsubscribeY();
+    };
+  }, []);
+
   return (
-    <section className="bg-[#101010] flex flex-col relative text-white text-center gap-4 py-[200px] px-10">
+    <section
+      ref={scrollRef}
+      className="bg-[#101010] flex flex-col relative text-white text-center gap-4 py-[200px] px-10"
+    >
       <div className="max-w-[1260px] w-full mx-auto relative">
         <div className="flex flex-wrap w-full gap-6 items-center px-4 lg:px-0 pb-10">
-          <h1 className="font-semibold self-start w-full text-left md:w-auto text-2xl md:text-[46px] text-[#86868b] mr-auto">
+          <motion.h1
+            className="font-semibold self-start w-full text-left md:w-auto text-2xl md:text-[46px] text-[#86868b] mr-auto"
+            initial={{ opacity: 0, y: 20 }}
+            animate={control1}
+          >
             일단 핵심부터.
-          </h1>
-          <div className="md:text-xl font-semibold text-[#0071e3] cursor-pointer hover:underline">
+          </motion.h1>
+          <motion.div
+            className="md:text-xl font-semibold text-[#0071e3] cursor-pointer hover:underline"
+            initial={{ opacity: 0, y: 20 }}
+            animate={control2}
+          >
             동영상 보기
-          </div>
-          <div className="md:text-xl font-semibold text-[#0071e3] cursor-pointer hover:underline">
+          </motion.div>
+          <motion.div
+            className="md:text-xl font-semibold text-[#0071e3] cursor-pointer hover:underline"
+            initial={{ opacity: 0, y: 20 }}
+            animate={control3}
+          >
             이벤트 시청하기
-          </div>
+          </motion.div>
         </div>
       </div>
       <div>carousel</div>
