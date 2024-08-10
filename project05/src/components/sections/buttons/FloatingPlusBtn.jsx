932bb0ad0b5e946eefeb32e5f1bfe2495abca74a
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-10 18:33:51 +0900

    feat: 버튼 스케일 애니메이션

--- project05/src/components/sections/buttons/FloatingPlusBtn.jsx
+++ project05/src/components/sections/buttons/FloatingPlusBtn.jsx
@@ -1,8 +1,7 @@
 "use client";
 
 import MoreIcon from "@/public/icons/more.svg";
-import { AnimatePresence, useAnimate, useInView } from "framer-motion";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence, useAnimate, useInView } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 export default function FloatingPlusBtn({ title }) {
@@ -14,18 +13,16 @@ export default function FloatingPlusBtn({ title }) {
 
   useEffect(() => {
     if (!isInView) return;
-    if (isInView) {
-      const animateBtn = async () => {
-        await animate(
-          "button",
-          { borderWidth: "20px" },
-          { transition: { delay: 1 } }
-        );
-        animate("button", { borderWidth: "0px" });
-      };
+    const animateBtn = async () => {
+      await animate(
+        "button",
+        { borderWidth: "20px" },
+        { transition: { delay: 1 } }
+      );
+      animate("button", { borderWidth: "0px" });
+    };
 
-      animateBtn();
-    }
+    animateBtn();
   }, [isInView, animate]);
 
   return (
@@ -38,10 +35,10 @@ export default function FloatingPlusBtn({ title }) {
           {isInView && (
             <motion.button
               className="backdrop-effect rounded-full border-[#016dda]"
-              style={{ backgroundColor: "rgb(66 66 69/70%)" }}
               initial={{ opacity: 0 }}
               animate={{
                 opacity: 1,
+                backgroundColor: "rgb(66 66 69/70%)",
                 transition: {
                   opacity: {
                     duration: 1,
@@ -51,6 +48,7 @@ export default function FloatingPlusBtn({ title }) {
               }}
               exit={{
                 opacity: 0,
+                backgroundColor: "rgb(66 66 69/70%)",
                 transition: {
                   opacity: {
                     duration: 0.3,
@@ -60,12 +58,57 @@ export default function FloatingPlusBtn({ title }) {
               }}
             >
               <div className="flex items-center p-2.5 min-h-[56px] min-w-[56px]">
-                <span className="text-white overflow-hidden whitespace-nowrap font-semibold text-sm">
+                <motion.span
+                  className="text-white overflow-hidden whitespace-nowrap font-semibold text-sm"
+                  initial={{
+                    opacity: 0,
+                    width: 0,
+                    marginInlineStart: "0px",
+                    marginInlineEnd: "0px",
+                  }}
+                  animate={{
+                    opacity: 1,
+                    width: "auto",
+                    marginInlineStart: "8px",
+                    marginInlineEnd: "12px",
+                    transition: {
+                      delay: 1,
+                    },
+                  }}
+                  exit={{
+                    opacity: 0,
+                    width: 0,
+                    marginInlineStart: "0px",
+                    marginInlineEnd: "0px",
+                    transition: {
+                      delay: 0,
+                    },
+                  }}
+                >
                   {title}
-                </span>
-                <span className=" bg-[#0071e3] rounded-full flex justify-center items-center text-white">
+                </motion.span>
+                <motion.span
+                  className=" bg-[#0071e3] rounded-full flex justify-center items-center text-white"
+                  initial={{ scale: 0, opacity: 0 }}
+                  animate={{
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                      duration: 0.3,
+                      delay: 0.5,
+                    },
+                  }}
+                  exit={{
+                    scale: 0,
+                    opacity: 0,
+                    transition: {
+                      duration: 0,
+                      delay: 0,
+                    },
+                  }}
+                >
                   <MoreIcon />
-                </span>
+                </motion.span>
               </div>
             </motion.button>
           )}
