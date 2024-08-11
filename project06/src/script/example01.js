0fb7cb0ad6ffe1b444cafa15cf318285a4ace081
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-11 21:28:25 +0900

    feat: scrollY 값으로 영상 조절하기

--- project06/src/script/example01.js
+++ project06/src/script/example01.js
@@ -1,7 +1,19 @@
-function pageExample01 () {
-  
+function pageExample01() {
+  const video = document.getElementById("video");
+
+  // scrollY 값으로 영상 조절하기
+  window.addEventListener("scroll", () => {
+    const time = (window.scrollY / 1000) * 4;
+    video.currentTime = time;
+  });
+
+  // video 초기화
+  addEventListener("load", () => {
+    video.pause();
+    video.currentTime = 0;
+  });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  pageExample01()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  pageExample01();
+});
