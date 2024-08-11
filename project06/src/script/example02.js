9975f74241cac71cd1ebad1bc14997656f65ae4f
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-11 21:36:54 +0900

    comment: 코드 정리

--- project06/src/script/example02.js
+++ project06/src/script/example02.js
@@ -7,35 +7,6 @@ function pageExample02() {
 
   canvas.width = 1158;
   canvas.height = 778;
-
-  // line
-  context.lineWidth = 10;
-
-  // Wall
-  context.strokeStyle = "brown";
-  context.strokeRect(75, 140, 150, 110); // x, y, w, h
-
-  // Door
-  context.fillRect(130, 190, 40, 60);
-
-  // Roof
-  context.strokeStyle = "red";
-  context.beginPath();
-  context.moveTo(50, 140);
-  context.lineTo(150, 60);
-  context.lineTo(250, 140);
-  context.closePath();
-  context.stroke();
-
-  // Cimeny
-  context.strokeStyle = "blue";
-  context.beginPath();
-  context.moveTo(200, 100);
-  context.lineTo(200, 60);
-  context.lineTo(220, 60);
-  context.lineTo(220, 115);
-  context.closePath();
-  context.stroke();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
