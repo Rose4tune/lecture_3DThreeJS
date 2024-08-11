c10b7f246a238165ea199d1115cee08c6b9eed27
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-11 22:13:19 +0900

    feat: 영상 이미지 강아지로 바꿔서 적용해보기

--- project06/src/script/example03.js
+++ project06/src/script/example03.js
@@ -1,10 +1,47 @@
-function pageExample03 () {
+function pageExample03() {
   const html = document.documentElement;
-  const canvas = document.getElementById("scrollAnimation");
+  const canvas = document.querySelector("canvas");
   const context = canvas.getContext("2d");
 
+  canvas.width = 1280;
+  canvas.height = 740;
+
+  const frameCount = 129;
+  const currentFrame = (index) =>
+    `assets/img/dog_${index.toString().padStart(3, "0")}.jpg`;
+
+  const img = new Image();
+  img.src = currentFrame(1);
+  img.onload = () => {
+    context.drawImage(img, 0, 0);
+  };
+
+  const updateImage = (index) => {
+    img.src = currentFrame(index);
+    context.drawImage(img, 0, index);
+  };
+
+  window.addEventListener("scroll", () => {
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.min(
+      frameCount - 1,
+      Math.ceil(scrollFraction * frameCount)
+    );
+
+    requestAnimationFrame(() => updateImage(frameIndex + 1));
+  });
+
+  const preloadImages = () => {
+    for (let i = 1; i < frameCount; i++) {
+      const img = new Image();
+      img.src = currentFrame(i);
+    }
+  };
+  preloadImages();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  pageExample03()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  pageExample03();
+});
